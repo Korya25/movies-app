46485dbbb735ef510ef0529fe5c9accb9ddda44d
Korya25 <enthrj3@gmail.com>
2025-03-06 01:22:45 +0200

    - add app_strings [ file ]

---
+++ lib/core/constant/app_strings.dart
@@ -0,0 +1,51 @@
+class AppStrings {
+  // App Info
+  static const String appTitle = 'Movies App';
+
+  // Navigation
+  static const String movies = 'Movies';
+  static const String shows = 'Shows';
+  static const String search = 'Search';
+  static const String watchlist = 'Watchlist';
+
+  // Sections
+  static const String popularMovies = 'Popular movies';
+  static const String topRatedMovies = 'Top rated movies';
+  static const String popularShows = 'Popular shows';
+  static const String topRatedShows = 'Top rated shows';
+
+  // Movie/Show Details
+  static const String story = 'Story';
+  static const String videos = 'Videos';
+  static const String cast = 'Cast';
+  static const String reviews = 'Reviews';
+  static const String similar = 'Similar';
+  static const String seasons = 'Seasons';
+  static const String season = 'Season';
+  static const String episodes = 'Episodes';
+  static const String episode = 'Episode';
+  static const String airDate = 'Air date:';
+  static const String lastEpisode = 'Last Episode';
+  static const String lastEpisodeOnAir = 'Last Episode on Air';
+
+  // Expandable Text
+  static const String showLess = 'Show less';
+  static const String showMore = 'Show more';
+
+  // Search
+  static const String searchText =
+      'By typing in search bar, Movia search in movies and series and then show you the best results.';
+  static const String searchHint = 'Search for Movies, Series...';
+  static const String noResults = 'No results';
+
+  // Watchlist
+  static const String watchlistIsEmpty = 'Watchlist is empty';
+  static const String watchlistText =
+      'After adding movies and series to watchlist, they will appear here.';
+
+  // Errors
+  static const String oops = 'Ooops';
+  static const String tryAgainLater = 'Please try again later';
+  static const String errorMessage = 'Something went wrong';
+  static const String tryAgain = 'try again';
+}
