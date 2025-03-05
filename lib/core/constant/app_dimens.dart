dce7ae911e37ae22130e9836bed87129f61dd647
Korya25 <enthrj3@gmail.com>
2025-03-06 01:20:25 +0200

    - add app_dimens [ file ]

---
+++ lib/core/constant/app_dimens.dart
@@ -0,0 +1,90 @@
+class Dimens {
+  // Margins
+  static const double margin2 = 2;
+  static const double margin4 = 4;
+  static const double margin6 = 6;
+  static const double margin8 = 8;
+  static const double margin10 = 10;
+  static const double margin12 = 12;
+  static const double margin14 = 14;
+  static const double margin16 = 16;
+  static const double margin18 = 18;
+  static const double margin20 = 20;
+
+  // Padding
+  static const double padding2 = 2;
+  static const double padding4 = 4;
+  static const double padding6 = 6;
+  static const double padding8 = 8;
+  static const double padding10 = 10;
+  static const double padding12 = 12;
+  static const double padding14 = 14;
+  static const double padding16 = 16;
+  static const double padding18 = 18;
+  static const double padding20 = 20;
+  static const double padding22 = 22;
+  static const double padding24 = 24;
+  static const double padding26 = 26;
+  static const double padding28 = 28;
+  static const double padding30 = 30;
+  static const double padding32 = 32;
+  static const double padding34 = 34;
+  static const double padding36 = 36;
+
+  // Border Radius
+  static const double borderRadius2 = 2;
+  static const double borderRadius4 = 4;
+  static const double borderRadius6 = 6;
+  static const double borderRadius8 = 8;
+  static const double borderRadius10 = 10;
+  static const double borderRadius12 = 12;
+  static const double borderRadius14 = 14;
+  static const double borderRadius16 = 16;
+  static const double borderRadius18 = 18;
+  static const double borderRadius20 = 20;
+  static const double borderRadius22 = 22;
+  static const double borderRadius24 = 24;
+  static const double borderRadius26 = 26;
+  static const double borderRadius28 = 28;
+  static const double borderRadius30 = 30;
+  static const double borderRadius32 = 32;
+  static const double borderRadius34 = 34;
+  static const double borderRadius36 = 36;
+
+  // Sizes
+  static const double size0 = 0;
+  static const double size1 = 1;
+  static const double size4 = 4;
+  static const double size6 = 6;
+  static const double size8 = 8;
+  static const double size10 = 10;
+  static const double size12 = 12;
+  static const double size14 = 14;
+  static const double size15 = 15;
+  static const double size16 = 16;
+  static const double size18 = 18;
+  static const double size20 = 20;
+  static const double size22 = 22;
+  static const double size24 = 24;
+  static const double size30 = 30;
+  static const double size36 = 36;
+  static const double size40 = 40;
+  static const double size44 = 44;
+  static const double size45 = 45;
+  static const double size60 = 60;
+  static const double size84 = 84;
+  static const double size100 = 100;
+  static const double size110 = 110;
+  static const double size120 = 120;
+  static const double size128 = 128;
+  static const double size140 = 140;
+  static const double size150 = 150;
+  static const double size160 = 160;
+  static const double size175 = 175;
+  static const double size190 = 190;
+  static const double size200 = 200;
+  static const double size220 = 220;
+  static const double size240 = 240;
+  static const double size400 = 400;
+  static const double size800 = 800;
+}
