e3f93f4ac738eae7787757b6ede00315723017a4
Korya25 <enthrj3@gmail.com>
2025-03-07 16:13:05 +0200

    add toprated tv shows screen

--- lib/features/shows/presentation/screens/top_rated_tv_shows_screen.dart
+++ lib/features/shows/presentation/screens/top_rated_tv_shows_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movies_ap_p/core/presentation/component/custom_app_bar.dart';
+import 'package:movies_ap_p/core/presentation/component/vertical_list_view.dart';
+import 'package:movies_ap_p/core/presentation/component/vertical_list_view_card.dart';
+import 'package:movies_ap_p/core/resources/constant/app_strings.dart';
+import 'package:movies_ap_p/core/resources/router/app_routes.dart';
 
 class TopRatedTvShowsScreen extends StatelessWidget {
   const TopRatedTvShowsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Top Rated Tv Shows Screen')),
+    return Scaffold(
+      appBar: CustomAppBar(title: AppStrings.topRatedTvShows),
+      body: SafeArea(child: TopRatedTvShowsWidget()),
+    );
+  }
+}
+
+class TopRatedTvShowsWidget extends StatelessWidget {
+  const TopRatedTvShowsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return VerticalListView(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return VerticalListViewCard(
+          onTap: () {
+            context.goNamed(AppRoutes.tvShowDetailsScreen);
+          },
+        );
+      },
     );
   }
 }
